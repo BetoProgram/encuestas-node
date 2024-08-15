8d0249b6267c0cfe88a2ef96a8ca8785972cfb13
BetoProgram <juancho34_8@msn.com>
2024-08-15 16:22:46 -0600

    se habilitan cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import morgan from 'morgan';
 import router from './router';
+import cors from 'cors';
 
 const server = express();
-
+server.use(cors());
 server.use(express.json())
 server.use(morgan('dev'));
 server.use('/api', router);
